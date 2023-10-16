543fbd0cb33e80656c063a2c7fa4d2554724ad57
Prajval K <prajval22@iisertvm.ac.in>
2023-10-16 19:59:29 +0530

    Add new proto

---
+++ 903144e7/index.js
@@ -0,0 +1,33 @@
+function close_nav() {
+    document.getElementById("nav").style.display = "none";
+}
+
+function open_nav() {
+    document.getElementById("nav").style.display = "flex";
+}
+
+function toggle(x) {
+  classname = '#' + x;
+  $(classname).slideToggle('slow'); 
+}
+
+const observerOptions = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.7
+  };
+  
+  function observerCallback(entries, observer) {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.replace('fadeOut', 'fadeIn');
+      } else {
+        entry.target.classList.replace('fadeIn', 'fadeOut');
+      }
+    });
+  }
+  
+  const observer = new IntersectionObserver(observerCallback, observerOptions);
+  
+  const fadeElms = document.querySelectorAll('.fade');
+  fadeElms.forEach(el => observer.observe(el));
